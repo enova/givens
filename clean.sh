62e513f73c88db59c89893a5f2a140e9735ed010
Mason Boeman <masonboeman@gmail.com>
2020-01-24 02:24:01 -0600

    fix scripts and add clean

---
+++ clean.sh
@@ -0,0 +1,7 @@
+rm -rf node_modules
+rm -rf dist
+rm -rf coverage
+rm -rf integration-tests/jest/node_modules
+rm -f integration-tests/jest/givens.tgz
+rm -rf integration-tests/mocha/node_modules
+rm -f integration-tests/mocha/givens.tgz
