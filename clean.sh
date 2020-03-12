0f249d6384f5d1388c675e05591ad2b73281d4a9
Mason Boeman <masonboeman@gmail.com>
2020-03-11 23:23:48 -0500

    move javascript tests to their own directory

--- clean.sh
+++ clean.sh
@@ -1,9 +1,13 @@
 rm -rf node_modules
 rm -rf dist
 rm -rf coverage
-rm -rf integration-tests/jest/node_modules
-rm -f integration-tests/jest/givens.tgz
-rm -rf integration-tests/mocha/node_modules
-rm -f integration-tests/mocha/givens.tgz
-rm -rf integration-tests/jasmine/node_modules
-rm -f integration-tests/jasmine/givens.tgz
+
+rm -rf integration-tests/javascript/jest/node_modules
+rm -f integration-tests/javascript/jest/givens.tgz
+rm -rf integration-tests/javascript/mocha/node_modules
+rm -f integration-tests/javascript/mocha/givens.tgz
+rm -rf integration-tests/javascript/jasmine/node_modules
+rm -f integration-tests/javascript/jasmine/givens.tgz
+
+rm -rf integration-tests/typescript/jest/node_modules
+rm -f integration-tests/typescript/jest/givens.tgz
--- integration-tests/jest/build-givens.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ./../../
-npm pack
-for f in *.tgz; do
-    mv $f integration-tests/jest/givens.tgz
-done
